f577f8695247fd68bc6392454fb33c80ca198d4c
renweizhukov <renwei2004@gmail.com>
2015-07-10 11:24:46 -0700

    Add detailed comments

--- L231_PowerOfTwo/Solution.cpp
+++ L231_PowerOfTwo/Solution.cpp
@@ -5,10 +5,18 @@ public:
     {
         if (n <= 0)
         {
+            // An integer can't be a power of two if it
+            // isn't positive.
             return false;
         }
         else
         {
+            // If a positive integer n is a power of two, 
+            // it has exactly one '1' in its binary 
+            // representation and n & (n - 1) will be zero; 
+            // otherwise, it has either 0 or more than one '1'
+            // in its binary representation and n & (n - 1) 
+            // will be non-zero.
             return !(n & (n - 1));
         }
     }
