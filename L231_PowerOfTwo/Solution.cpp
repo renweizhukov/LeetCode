5d13649f142b9a18494ced21e5f65d350f9ad96e
renweizhukov <renwei2004@gmail.com>
2015-07-10 11:14:42 -0700

    Solution to 231 Power of Two

---
+++ L231_PowerOfTwo/Solution.cpp
@@ -0,0 +1,15 @@
+class Solution
+{
+public:
+    bool isPowerOfTwo(int n)
+    {
+        if (n <= 0)
+        {
+            return false;
+        }
+        else
+        {
+            return !(n & (n - 1));
+        }
+    }
+};
