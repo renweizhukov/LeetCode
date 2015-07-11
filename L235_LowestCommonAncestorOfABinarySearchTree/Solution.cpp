e41935eae8cd8867f33a0e35cb6d14cfcd8bfe31
Wei Ren <renwei2004@gmail.com>
2015-07-11 16:57:43 -0700

    Add detailed comments

--- L235_LowestCommonAncestorOfABinarySearchTree/Solution.cpp
+++ L235_LowestCommonAncestorOfABinarySearchTree/Solution.cpp
@@ -17,18 +17,23 @@ public:
         {
             if ((curr == p) || (curr == q))
             {
+                // The lowest common ancestor is either p or q.
                 return curr;
             }
             else if ((p->val <= curr->val) && (q->val <= curr->val))
             {
+                // Both p and q are in the left subtree.
                 curr = curr->left;
             }
             else if ((p->val > curr->val) && (q->val > curr->val))
             {
+                // Both p and q are in the right subtree.
                 curr = curr->right;
             }
             else
             {
+                // p and q are in different subtrees, so curr is the 
+                // lowest common ancestor.
                 return curr;
             }
         }
