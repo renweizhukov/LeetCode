b03f31d6fdf3545133e51f9643b4b33eb0563128
renweizhukov <renwei2004@gmail.com>
2015-07-17 17:21:45 -0700

    Add detailed comments

--- L238_ProductOfArrayExceptSelf/Solution.cpp
+++ L238_ProductOfArrayExceptSelf/Solution.cpp
@@ -6,6 +6,8 @@ public:
         int n = nums.size();
         vector<int> res(n, 1);
 
+        // For each nums[i], calculate the product of elements 
+        // before i.
         int productBefore = nums[0];
         for (int i = 1; i < n; i++)
         {
@@ -13,6 +15,10 @@ public:
             productBefore *= nums[i];
         }
 
+        // For each nums[i], calculate the product of elements 
+        // after i, multiple it by the product of elements 
+        // before i, and obtain the product of the array except 
+        // nums[i].
         int productAfter = nums[n - 1];
         for (int i = n - 2; i >= 0; i--)
         {
