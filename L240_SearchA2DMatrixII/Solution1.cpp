b358dbdb192d598737f7ace3b8ae69a546a19d9a
renweizhukov <renwei2004@gmail.com>
2015-07-28 17:10:03 -0700

    Add detailed comments

--- L240_SearchA2DMatrixII/Solution1.cpp
+++ L240_SearchA2DMatrixII/Solution1.cpp
@@ -6,15 +6,25 @@ public:
         int cntRows = matrix.size();
         if (cntRows == 0)
         {
+            // The matrix has zero row.
             return false;
         }
 
         int cntColumns = matrix[0].size();
         if (cntColumns == 0)
         {
+            // The matrix has zero column.
             return false;
         }
 
+        // Compare the top right element with target. 
+        // (1) If they are equal, we have found the target. 
+        // (2) If the top right element is smaller than target, 
+        //     we can skip all the elements in the same row 
+        //     since they can't be larger than the top right element.
+        // (3) If the top right element is larger than target,
+        //     we can skip all the elements in the same column
+        //     since they can't be smaller than the top right element.
         int rowIndex = 0;
         int columnIndex = cntColumns - 1;
         while ((rowIndex < cntRows) && (columnIndex >= 0))
